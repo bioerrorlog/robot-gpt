8cfde7f6288133b0c755611e7b9b7667c17367c2
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-03-28 21:36:41 +0900

    add: camera tests

---
+++ tests/test_robot_gpt.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+from robot_gpt.main import capture_image
+
+
+@pytest.mark.camera
+def test_capture_image_success():
+    image_filename = "test_captured_image.jpg"
+
+    result = capture_image(image_filename)
+
+    assert result == image_filename
+    assert os.path.exists(image_filename)
+
+    # Clean up the test image file.
+    os.remove(image_filename)

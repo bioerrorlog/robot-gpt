41ea77d0092d59cb68da006ff0b8fb8a67e8004e
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-09 22:30:43 +0900

    refactor: separate hardware tests

--- tests/test_robot_gpt.py
+++ tests/test_robot_gpt.py
@@ -1,40 +1,10 @@
-import os
 import json
 import pytest
-from robot_gpt.hardware import (
-    capture_image,
-    recognize_objects,
-)
+
 from robot_gpt.robot import (
     Role,
     ChatWithGPT,
 )
-
-
-@pytest.mark.camera
-def test_capture_image_success():
-    """A connected camera required"""
-    image_filename = "outputs/test_captured_image.jpg"
-
-    result = capture_image(image_filename)
-
-    assert result == image_filename
-    assert os.path.exists(image_filename)
-
-    # Clean up the test image file.
-    os.remove(image_filename)
-
-
-@pytest.mark.recognize
-def test_recognize_objects_success():
-    """Download pre-trained model before running this test. See README."""
-    image_path = "outputs/unittest.jpg"
-
-    objects = recognize_objects(image_path)
-
-    assert isinstance(objects, list)
-    assert all(isinstance(item, str) for item in objects)
-    assert len(objects) >= 1
 
 
 @ pytest.mark.chatgpt

d0916afd962b1e2f5ed60957301de73f998789ed
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-11 16:31:37 +0900

    update: delete redundancy

--- robot_gpt/main.py
+++ robot_gpt/main.py
@@ -1,29 +1,13 @@
-import logging
 from robot_gpt.robot import RobotGPT
-from robot_gpt.hardware import (
-    capture_image,
-    recognize_objects,
-)
-
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-logger.addHandler(console_handler)
 
 
 def main() -> None:
-    image_path = capture_image("outputs/captured_image.jpg")
-    logger.info(f"Image captured: {image_path}")
+    robot = RobotGPT()
 
-    objects = recognize_objects(image_path)
-    logger.info(f"Objects detected: {objects}")
+    robot.recognize(45, -30)
 
-    chatbot = RobotGPT()
-    message = chatbot.generate_response()
-    print(f"ChatGPT says: {message}")
+    message = robot.generate_response()
+    print(f"RobotGPT says: {message}")
 
 
 if __name__ == "__main__":

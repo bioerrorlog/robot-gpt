5cbc12e17ec73f07992e9af73c76300f70d56424
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-12 22:12:02 +0900

    unify system prompts

--- robot_gpt/robot.py
+++ robot_gpt/robot.py
@@ -28,11 +28,13 @@ class Role(Enum):
 class RobotGPT:
     def __init__(self, horizontal: int = 0, vertical: int = 0):
         self._prompts = [
-            {"role": Role.SYSTEM.value, "content": "You are a robot with a camera, composed of 2 servo motors: horizontal & vertical"},
-            {"role": Role.SYSTEM.value, "content": "Horizontal: min -90 left, max 90 right"},
-            {"role": Role.SYSTEM.value, "content": "Vertical: min -90 down, max 90 up"},
-            {"role": Role.SYSTEM.value, "content": "Your behavior principles: [curiosity, inquisitiveness, playfulness]"},
-            {"role": Role.SYSTEM.value, "content": 'Your answer MUST be in this JSON format: {"NextServoMotor": {"Horizontal": int(-90~90), "Vertical": int(-90~90)} "FreeTalk": string}'},
+            {"role": Role.SYSTEM.value, "content": """
+You are a robot with a camera, composed of 2 servo motors: horizontal & vertical.
+Horizontal: min -90 left, max 90 right.
+Vertical: min -90 down, max 90 up.
+Your behavior principles: [curiosity, inquisitiveness, playfulness].
+Your answer MUST be in this JSON format: {"NextServoMotor": {"Horizontal": int(-90~90), "Vertical": int(-90~90)} "FreeTalk": string}
+"""},
         ]
         self._horizontal = horizontal
         self._vertical = vertical
